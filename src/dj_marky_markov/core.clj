6b65a7b28471516327f32c8efdf1b045d8f6454d
Nick Nichols <nick.nichols@rate.com>
2019-01-31 10:55:19 -0600

    Add sliding window functionality

--- src/dj_marky_markov/core.clj
+++ src/dj_marky_markov/core.clj
@@ -1,7 +1,14 @@
 (ns dj-marky-markov.core
+  (require [clojure.string :as cs])
   (:gen-class))
+
+(defn string-to-sliding-window
+  [string window-length]
+  (partition window-length 1 (cs/split string #"\s+")))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [path (first args)
+        window-length (Integer/parseInt (second args))]
+    (println (string-to-sliding-window (slurp path) window-length))))
