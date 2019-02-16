c3f6ee1f7852d1095b0a8996ce22383777ab5d72
Nick A. Nichols <nichols1991@gmail.com>
2019-02-16 10:29:00 -0600

    Add basic tests

--- src/dj_marky_markov/core.clj
+++ src/dj_marky_markov/core.clj
@@ -1,48 +1,65 @@
 (ns dj-marky-markov.core
+  "A simple Markov Chain for text generation in Clojure"
   (:require [clojure.string :as cs])
   (:gen-class))
 
 (def first-capital-letter-regex
+  "Regex pattern to find words that start sentences"
   (re-pattern "^[A-Z]"))
 
-;;HACK - This is fuzzy detection for string terminators. Needs to be more robust
 (def punctuation-regex
+  "This is a loose regex detection for string terminators. Needs to be more robust"
   (re-pattern "[\\.\\?\\!]"))
 
 (defn concat-with-space
+  "Return `s1` concatenated with `s2` with a space between the two strings"
   [s1 s2]
   (str s1 " " s2))
 
 (defn search-text
+  "Take `window-length` words from the end of `sentence`
+   to use as the look-up for the next markov transition"
   [stem window-length]
   (let [trailing-part (take-last window-length (cs/split stem #"\s"))]
     (cs/trim (reduce concat-with-space trailing-part))))
 
 (defn string-to-sliding-window
+  "Convert `string` into a series of sliding windows that will be sliced into
+   a window and a transition word, hence incrementing the partition window."
   [string window-length]
   (partition (inc window-length) 1 (cs/split string #"\s+")))
 
 (defn single-window-to-tuple
+  "Split `window` into a look-up and a transition tuple"
   [window]
   (cons (reduce concat-with-space (butlast window)) (list (last window))))
 
 (defn add-entry
+  "Update `dictionary` with an `entry` tuple.
+   If the look-up already exists, cons the current transition to the list of transitions"
   [dictionary entry]
   (update dictionary (first entry) #(cons (second entry) %)))
 
 (defn build-markov-dictionary
+  "Collapse `tuple-set` into a look-up dictionary"
   [tuple-set]
   (reduce add-entry {} tuple-set))
 
 (defn starts-sentence?
+  "Boolean predicate that returns true iff the first word of `text-window`
+   starts with a capital letter"
   [text-window]
   (boolean (re-find first-capital-letter-regex (first text-window))))
 
 (defn sentence-ended?
+  "Boolean predicate that returns true iff `text` contains terminal punctuation"
   [text]
   (boolean (re-find punctuation-regex text)))
 
 (defn markov-sentence
+  "Build a sentence starting with a random element in `sentence-starters`, and
+   recursively navigate the markov model in `sentence-bodies`, whose transition windows
+   are sized by `window-length`"
   [sentence-starters sentence-bodies window-length]
   (loop [sentence (rand-nth (keys sentence-starters))]
     (if (sentence-ended? sentence)
@@ -55,11 +72,12 @@
         (recur (concat-with-space sentence added-text))))))
 
 (defn markov-sentences
+  "Generate `sentences` repeatedly"
   [sentence-starters sentence-bodies window-length sentences]
   (take sentences (repeatedly #(markov-sentence sentence-starters sentence-bodies window-length))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Try me out!"
   [& args]
   (let [path (first args)
         window-length (Integer/parseInt (second args))
--- test/dj_marky_markov/core_test.clj
+++ test/dj_marky_markov/core_test.clj
@@ -1,7 +1,43 @@
 (ns dj-marky-markov.core-test
   (:require [clojure.test :refer :all]
-            [dj-marky-markov.core :refer :all]))
+            [dj-marky-markov.core :as dmm]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest concat-with-space-test
+  (testing "Concatenation of strings always includes a space"
+    (is (= "Hello World"   (dmm/concat-with-space "Hello" "World")))
+    (is (= "Hello, World"  (dmm/concat-with-space "Hello," "World")))
+    (is (= "Hello World!"  (dmm/concat-with-space "Hello" "World!")))
+    (is (= "Hello, World!" (dmm/concat-with-space "Hello," "World!")))))
+
+(deftest search-text-test
+  (testing "A string can be sliced to the last n words"
+    (let [test-text "If on a winter's night a traveler"]
+      (is (thrown? clojure.lang.ArityException (dmm/search-text test-text 0)))
+      (is (= (dmm/search-text test-text 1) "traveler"))
+      (is (= (dmm/search-text test-text 2) "a traveler"))
+      (is (= (dmm/search-text test-text 3) "night a traveler"))
+      (is (= (dmm/search-text test-text 4) "winter's night a traveler"))
+      (is (= (dmm/search-text test-text 7) test-text))
+      (is (= (dmm/search-text test-text 8) test-text)))))
+
+(deftest string-to-sliding-window-test
+  (testing "A string may be sliced into multiple text/transition windows"
+    (let [test-text "It was a pleasure to burn."]
+      (is (= (dmm/string-to-sliding-window test-text 0) ['("It") '("was") '("a") '("pleasure") '("to") '("burn.")]))
+      (is (= (dmm/string-to-sliding-window test-text 1) ['("It" "was") '("was" "a") '("a" "pleasure") '("pleasure" "to") '("to" "burn.")]))
+      (is (= (dmm/string-to-sliding-window test-text 5) ['("It" "was" "a" "pleasure" "to" "burn.")]))
+      (is (empty? (dmm/string-to-sliding-window test-text 6))))))
+
+(deftest sentence-predicate-test
+  (testing "Ensure sentence predicates accurately track English behavior"
+    (is (dmm/starts-sentence? ["A" "screaming" "comes" "across" "the" "sky."]))
+    (is (not (dmm/starts-sentence? ["124" "was" "spiteful."])))
+    (is (not (dmm/starts-sentence? ["—Money" "." "." "." "in" "a" "voice" "that" "rustled."])))
+    (is (not (dmm/starts-sentence? ["the" "moment" "one" "learns" "English," "complications" "set" "in."])))
+    (is (dmm/sentence-ended? "Dr. Weiss, at forty, knew that her life had been ruined by literature"))
+    (is (dmm/sentence-ended? "The story so far: In the beginning the Universe was created."))
+    (is (dmm/sentence-ended? "Call me Ishmael."))
+    (is (dmm/sentence-ended? "Where now? Who now? When now?"))
+    (is (dmm/sentence-ended? "Stop! cried the groaning old man at last, Stop! I did not drag my father beyond this tree"))
+    (is (not (dmm/sentence-ended? "The ting go skraaa")))
+    (is (not (dmm/sentence-ended? "All this happened, more or less-")))))
